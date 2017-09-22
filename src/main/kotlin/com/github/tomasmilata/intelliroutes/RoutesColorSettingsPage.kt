873b9f818e61d3fc2c4c8fabc99cbc21f5bc5857
Tomáš Milata <tomas.milata@gmail.com>
2017-09-22 23:07:01 +0100

    Fix a demo text on the colors settings page

--- src/main/kotlin/com/github/tomasmilata/intelliroutes/RoutesColorSettingsPage.kt
+++ src/main/kotlin/com/github/tomasmilata/intelliroutes/RoutesColorSettingsPage.kt
@@ -18,18 +18,10 @@ class RoutesColorSettingsPage : ColorSettingsPage {
     }
 
     override fun getDemoText(): String {
-        return "# You are reading the \".properties\" entry.\n" +
-                "! The exclamation mark can also mark text as comments.\n" +
-                "website = http://en.wikipedia.org/\n" +
-                "language = English\n" +
-                "# The backslash below tells the application to continue reading\n" +
-                "# the value onto the next line.\n" +
-                "message = Welcome to \\\n" +
-                "          Wikipedia!\n" +
-                "# Add spaces to the key\n" +
-                "key\\ with\\ spaces = This is the value that could be looked up with the key \"key with spaces\".\n" +
-                "# Unicode\n" +
-                "tab : \\u0009"
+        return """
+            # Gets a single user with the given ID.
+            GET    /user/:userId    UserController.get(userId: String)
+        """.trimIndent()
     }
 
     override fun getAdditionalHighlightingTagToDescriptorMap(): Map<String, TextAttributesKey>? {
@@ -49,6 +41,10 @@ class RoutesColorSettingsPage : ColorSettingsPage {
     }
 
     companion object {
-        private val DESCRIPTORS = arrayOf(AttributesDescriptor("HTTP Verb", RoutesSyntaxHighlighter.VERB), AttributesDescriptor("Path", RoutesSyntaxHighlighter.PATH), AttributesDescriptor("Call", RoutesSyntaxHighlighter.CALL))
+        private val DESCRIPTORS = arrayOf(
+                AttributesDescriptor("HTTP Verb", RoutesSyntaxHighlighter.VERB),
+                AttributesDescriptor("Path", RoutesSyntaxHighlighter.PATH),
+                AttributesDescriptor("Call", RoutesSyntaxHighlighter.CALL)
+        )
     }
 }
