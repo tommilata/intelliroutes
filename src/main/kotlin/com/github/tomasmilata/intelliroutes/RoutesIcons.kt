c2bcd2e5f1049756c7323b83e45a350ae0367198
Tomáš Milata <tomas.milata@gmail.com>
2017-10-20 22:28:57 +0100

    Clean up unused imports

--- src/main/kotlin/com/github/tomasmilata/intelliroutes/RoutesIcons.kt
+++ src/main/kotlin/com/github/tomasmilata/intelliroutes/RoutesIcons.kt
@@ -2,8 +2,6 @@ package com.github.tomasmilata.intelliroutes
 
 import com.intellij.openapi.util.IconLoader
 
-import javax.swing.*
-
 object RoutesIcons {
     val FILE = IconLoader.getIcon("/icons/jar-gray.png")
 }
