897dad8af6097d5b090f450bbb6f9d2b806755b1
Tomáš Milata <tomas.milata@gmail.com>
2017-11-06 13:16:22 +0000

    Improve toString() in token types for better syntax error reporting

--- src/main/kotlin/com/github/tomasmilata/intelliroutes/psi/RoutesTokenType.kt
+++ src/main/kotlin/com/github/tomasmilata/intelliroutes/psi/RoutesTokenType.kt
@@ -2,11 +2,30 @@ package com.github.tomasmilata.intelliroutes.psi
 
 import com.intellij.psi.tree.IElementType
 import com.github.tomasmilata.intelliroutes.RoutesLanguage
+import com.github.tomasmilata.intelliroutes.psi.RoutesTypes.*
 import org.jetbrains.annotations.*
 
 class RoutesTokenType(@NonNls debugName: String) : IElementType(debugName, RoutesLanguage.INSTANCE) {
 
     override fun toString(): String {
-        return "RoutesTokenType." + super.toString()
+        return when (this) {
+            COLON -> ":"
+            COMMA -> ","
+            OPENING_PARENTHESIS -> "("
+            CLOSING_PARENTHESIS -> ")"
+            ARGUMENT_NAME -> "argument name"
+            ARGUMENT_TYPE -> "argument type"
+            ARGUMENT_VALUE -> "argument value"
+            COMMENT -> "comment"
+            CONTROLLER_METHOD -> "controller method"
+            EOL -> "new line"
+            EQ -> "="
+            PATH_PARAMETER -> "path parameter"
+            SLASH -> "/"
+            STATIC_PATH_SEGMENT -> "static path segment"
+            VERB -> "HTTP verb"
+            WILDCARD_PARAMETER -> "wildcard parameter"
+            else -> super.toString()
+        }
     }
 }
