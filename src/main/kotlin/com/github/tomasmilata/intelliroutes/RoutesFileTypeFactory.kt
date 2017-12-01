3cf74035d5941e46faef9ac416115faa8aab3e31
Tomáš Milata <tomas.milata@gmail.com>
2017-12-01 11:48:57 +0000

    Associate both "routes" and "*.routes" files with the editor

--- src/main/kotlin/com/github/tomasmilata/intelliroutes/RoutesFileTypeFactory.kt
+++ src/main/kotlin/com/github/tomasmilata/intelliroutes/RoutesFileTypeFactory.kt
@@ -1,9 +1,13 @@
 package com.github.tomasmilata.intelliroutes
 
-import com.intellij.openapi.fileTypes.*
+import com.intellij.openapi.fileTypes.ExactFileNameMatcher
+import com.intellij.openapi.fileTypes.ExtensionFileNameMatcher
+import com.intellij.openapi.fileTypes.FileTypeConsumer
+import com.intellij.openapi.fileTypes.FileTypeFactory
 
 class RoutesFileTypeFactory : FileTypeFactory() {
     override fun createFileTypes(fileTypeConsumer: FileTypeConsumer) {
         fileTypeConsumer.consume(RoutesFileType.INSTANCE, ExactFileNameMatcher("routes"))
+        fileTypeConsumer.consume(RoutesFileType.INSTANCE, ExtensionFileNameMatcher("routes"))
     }
 }
