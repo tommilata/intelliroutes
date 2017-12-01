629b4d9614cc65645c40834b301e0e1203e5af3d
Tomáš Milata <tomas.milata@gmail.com>
2017-12-01 18:52:52 +0000

    Get Java files for code completion from the index for better performace

--- src/main/kotlin/com/github/tomasmilata/intelliroutes/RoutesCompletionContributor.kt
+++ src/main/kotlin/com/github/tomasmilata/intelliroutes/RoutesCompletionContributor.kt
@@ -3,12 +3,17 @@ package com.github.tomasmilata.intelliroutes
 import com.github.tomasmilata.intelliroutes.psi.RoutesTypes
 import com.intellij.codeInsight.completion.*
 import com.intellij.codeInsight.lookup.LookupElementBuilder
+import com.intellij.ide.highlighter.JavaFileType
+import com.intellij.openapi.project.Project
 import com.intellij.patterns.PlatformPatterns
+import com.intellij.psi.PsiJavaFile
+import com.intellij.psi.PsiManager
+import com.intellij.psi.search.FileTypeIndex
 import com.intellij.psi.search.ProjectScope
-import com.intellij.psi.search.searches.AllClassesSearch
 import com.intellij.psi.util.InheritanceUtil.isInheritor
 import com.intellij.psi.util.PsiTypesUtil
 import com.intellij.util.ProcessingContext
+import com.intellij.util.indexing.FileBasedIndex
 
 
 class RoutesCompletionContributor : CompletionContributor() {
@@ -44,24 +49,36 @@ class RoutesCompletionContributor : CompletionContributor() {
                                                 context: ProcessingContext,
                                                 resultSet: CompletionResultSet) {
                         val project = parameters.originalFile.project
-                        val searchScope = ProjectScope.getContentScope(project)
-                        val query = AllClassesSearch.search(searchScope, project)
 
-                        query
-                            .flatMap { cls -> cls.methods.toList() }
-                            .forEach { method ->
+                        val javaClasses = javaFiles(project).flatMap { javaFile ->
+                            javaFile.classes.toList()
+                        }
+
+                        javaClasses.forEach { cls ->
+                            cls.methods.forEach { method ->
                                 val returnType = PsiTypesUtil.getPsiClass(method.returnType)
                                 if (isInheritor(returnType, "play.api.mvc.Action")) {
-                                    val containingClass = method.containingClass
-                                    if (containingClass != null) {
-                                        val lookupElement = LookupElementBuilder.create("${containingClass.qualifiedName}.${method.name}")
-                                        resultSet.addElement(lookupElement)
-                                    }
+                                    val lookupElement = LookupElementBuilder.create("${cls.qualifiedName}.${method.name}")
+                                    resultSet.addElement(lookupElement)
                                 }
                             }
-
+                        }
                     }
                 }
+
+        private fun javaFiles(project: Project): List<PsiJavaFile> {
+
+            val index = FileBasedIndex.getInstance()
+            val searchScope = ProjectScope.getContentScope(project)
+
+            val virtualFiles = index.getContainingFiles(
+                    FileTypeIndex.NAME, JavaFileType.INSTANCE, searchScope)
+
+
+            return virtualFiles.mapNotNull { virtualFile ->
+                PsiManager.getInstance(project).findFile(virtualFile) as PsiJavaFile?
+            }
+        }
 
     }
 }
