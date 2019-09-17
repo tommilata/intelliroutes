7a9f95d69ac44e67da852d6a71c2922c8282543f
Tomáš Milata <tomas.milata@gmail.com>
2019-09-17 23:34:56 +0100

    Add a simple parser unit test

---
+++ src/test/kotlin/com/github/tomasmilata/intelliroutes/ParserTest.kt
@@ -0,0 +1,23 @@
+package com.github.tomasmilata.intelliroutes
+
+import com.intellij.testFramework.ParsingTestCase
+
+
+class Simple: BaseTestCase() {
+
+    fun testSimple() {
+        doTest(true)
+    }
+}
+
+abstract class BaseTestCase : ParsingTestCase("", "routes", RoutesParserDefinition()) {
+
+
+    override fun getTestDataPath(): String {
+        return "src/test/resources/parser-tests"
+    }
+
+    override fun includeRanges(): Boolean {
+        return true
+    }
+}
