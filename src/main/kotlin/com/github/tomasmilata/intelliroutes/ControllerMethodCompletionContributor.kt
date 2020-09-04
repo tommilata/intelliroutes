929d62856d7abdfc017a0ee7bf7b9c4d588ac0d1
Tomáš Milata <tomas.milata@gmail.com>
2020-09-05 00:49:14 +0200

    Prioritize play.mvc.Action suggestions

--- src/main/kotlin/com/github/tomasmilata/intelliroutes/ControllerMethodCompletionContributor.kt
+++ src/main/kotlin/com/github/tomasmilata/intelliroutes/ControllerMethodCompletionContributor.kt
@@ -2,14 +2,19 @@ package com.github.tomasmilata.intelliroutes
 
 import com.intellij.codeInsight.completion.CompletionParameters
 import com.intellij.codeInsight.completion.CompletionResultSet
+import com.intellij.codeInsight.completion.PrioritizedLookupElement
 import com.intellij.codeInsight.lookup.LookupElementBuilder
 import com.intellij.openapi.fileTypes.LanguageFileType
 import com.intellij.openapi.project.Project
 import com.intellij.openapi.vfs.VirtualFile
 import com.intellij.psi.JavaPsiFacade
+import com.intellij.psi.PsiClass
 import com.intellij.psi.PsiClassOwner
+import com.intellij.psi.PsiMethod
 import com.intellij.psi.search.FileTypeIndex
 import com.intellij.psi.search.ProjectScope
+import com.intellij.psi.util.InheritanceUtil.isInheritorOrSelf
+import com.intellij.psi.util.PsiTypesUtil
 
 object ControllerMethodCompletionContributor {
 
@@ -22,40 +27,47 @@ object ControllerMethodCompletionContributor {
         val packageNames = filesToSearch.map { it.packageName }
         val classes = filesToSearch.flatMap { it.classes.toList() }
 
+        val playAction = playActionType(parameters)
+
+        fun addPrioritizedSuggestion(suggestion: String, priority: Double) {
+            val lookupElement = LookupElementBuilder.create(suggestion)
+            val prioritizedLookupElement = PrioritizedLookupElement.withPriority(lookupElement, priority)
+            resultSet.addElement(prioritizedLookupElement)
+        }
+
+        fun addSuggestion(fullSuggestion: String) {
+            val suggestedSuffix = fullSuggestion.removePrefix(enteredText)
+            if (!suggestedSuffix.contains('.')) { // only add suffixes until the next '.'
+                addPrioritizedSuggestion(fullSuggestion, 0.0)
+            }
+        }
+
+        fun addMethodSuggestion(cls: PsiClass, method: PsiMethod) {
+            val fullSuggestion = "${cls.qualifiedName}.${method.name}"
+
+            val suggestedSuffix = fullSuggestion.removePrefix(enteredText)
+            if (!suggestedSuffix.contains('.')) { // only add suffixes until the next '.'
+                val returnType = PsiTypesUtil.getPsiClass(method.returnType)
+
+                val priority =  if (isInheritorOrSelf(returnType, playAction, true))  1.0 else 0.0
+                addPrioritizedSuggestion(fullSuggestion, priority)
+            }
+        }
+
+        packageNames.forEach { addSuggestion(it) }
+        classes.forEach { cls ->
+            addSuggestion(cls.qualifiedName.toString())
+        }
+        classes.forEach { cls ->
+            cls.allMethods.forEach { method -> addMethodSuggestion(cls, method) }
+        }
+    }
+
+    private fun playActionType(parameters: CompletionParameters): PsiClass? {
         val project = parameters.originalFile.project
         val projectWithLibrariesScope = ProjectScope.getAllScope(project)
         val psiFacade = JavaPsiFacade.getInstance(project)
-
-
-//        psiFacade.findPackage()
-
-//        val playAction = psiFacade.findClass("play.api.mvc.Action", projectWithLibrariesScope)
-
-        fun add(fullSuggestion: String) {
-            val suggestedSuffix  = fullSuggestion.removePrefix(enteredText)
-            if (!suggestedSuffix.contains('.')) {
-                // only add suffixes until the next '.'
-                resultSet.addElement(LookupElementBuilder.create(fullSuggestion))
-            }
-        }
-
-        packageNames.forEach { add(it) }
-        classes.forEach { cls ->
-            add(cls.qualifiedName.toString())
-        }
-        classes.forEach { cls ->
-            cls.allMethods.forEach { method ->
-//                val returnType = PsiTypesUtil.getPsiClass(method.returnType)
-//                if (isInheritorOrSelf(returnType, playAction, true)) {
-//                    val name = "${cls.qualifiedName}.${method.name}"
-//                    val lookupElement = LookupElementBuilder.create(name)
-//                    resultSet.addElement(lookupElement)
-//                }
-                val fqMethodName = "${cls.qualifiedName}.${method.name}"
-//                println("method: $fqMethodName")
-                add(fqMethodName)
-            }
-        }
+        return psiFacade.findClass("play.api.mvc.Action", projectWithLibrariesScope)
     }
 
     fun virtualFiles(project: Project, fileType: LanguageFileType): Collection<VirtualFile> {
