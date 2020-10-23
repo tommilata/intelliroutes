019e3adf94497dec447c67265d0bbd86aa9683ca
Tomáš Milata <tomas.milata@gmail.com>
2020-10-23 23:56:03 +0200

    Optimize completion

--- src/main/kotlin/com/github/tomasmilata/intelliroutes/ControllerMethodCompletionContributor.kt
+++ src/main/kotlin/com/github/tomasmilata/intelliroutes/ControllerMethodCompletionContributor.kt
@@ -4,14 +4,7 @@ import com.intellij.codeInsight.completion.CompletionParameters
 import com.intellij.codeInsight.completion.CompletionResultSet
 import com.intellij.codeInsight.completion.PrioritizedLookupElement
 import com.intellij.codeInsight.lookup.LookupElementBuilder
-import com.intellij.openapi.fileTypes.LanguageFileType
-import com.intellij.openapi.project.Project
-import com.intellij.openapi.vfs.VirtualFile
-import com.intellij.psi.JavaPsiFacade
-import com.intellij.psi.PsiClass
-import com.intellij.psi.PsiClassOwner
-import com.intellij.psi.PsiMethod
-import com.intellij.psi.search.FileTypeIndex
+import com.intellij.psi.*
 import com.intellij.psi.search.ProjectScope
 import com.intellij.psi.util.InheritanceUtil.isInheritorOrSelf
 import com.intellij.psi.util.PsiTypesUtil
@@ -29,37 +22,26 @@ object ControllerMethodCompletionContributor {
 
         val playAction = playActionType(parameters)
 
-        fun addPrioritizedSuggestion(suggestion: String, priority: Double) {
+        fun suggest(suggestion: String, priority: Double) {
             val lookupElement = LookupElementBuilder.create(suggestion)
             val prioritizedLookupElement = PrioritizedLookupElement.withPriority(lookupElement, priority)
             resultSet.addElement(prioritizedLookupElement)
         }
 
-        fun addSuggestion(fullSuggestion: String) {
-            val suggestedSuffix = fullSuggestion.removePrefix(enteredText)
-            if (!suggestedSuffix.contains('.')) { // only add suffixes until the next '.'
-                addPrioritizedSuggestion(fullSuggestion, 0.0)
-            }
-        }
-
         fun addMethodSuggestion(cls: PsiClass, method: PsiMethod) {
-            val fullSuggestion = "${cls.qualifiedName}.${method.name}"
+            val fullMethod = "${cls.qualifiedName}.${method.name}"
+            val returnType = PsiTypesUtil.getPsiClass(method.returnType)
+            val priority = if (isInheritorOrSelf(returnType, playAction, true)) 1.0 else 0.0
+            suggest(fullMethod, priority)
+        }
 
-            val suggestedSuffix = fullSuggestion.removePrefix(enteredText)
-            if (!suggestedSuffix.contains('.')) { // only add suffixes until the next '.'
-                val returnType = PsiTypesUtil.getPsiClass(method.returnType)
-
-                val priority =  if (isInheritorOrSelf(returnType, playAction, true))  1.0 else 0.0
-                addPrioritizedSuggestion(fullSuggestion, priority)
+        packageNames.forEach { suggest(it, priority = 0.0) }
+        classes.mapNotNull { it.qualifiedName }.forEach { suggest(it, priority = 0.0) }
+        classes.forEach { cls ->
+            val className = cls.qualifiedName
+            if (className != null && enteredText.startsWith(className)) {
+                cls.allMethods.forEach { method -> addMethodSuggestion(cls, method) }
             }
-        }
-
-        packageNames.forEach { addSuggestion(it) }
-        classes.forEach { cls ->
-            addSuggestion(cls.qualifiedName.toString())
-        }
-        classes.forEach { cls ->
-            cls.allMethods.forEach { method -> addMethodSuggestion(cls, method) }
         }
     }
 
