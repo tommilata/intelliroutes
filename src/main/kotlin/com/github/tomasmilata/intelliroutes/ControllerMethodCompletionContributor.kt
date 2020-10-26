29b11d976a58647d06148a89d77361e4eff3b1ab
Tomáš Milata <tomas.milata@gmail.com>
2020-10-26 09:45:17 +0100

    Show icons next to controller class and method suggestions

--- src/main/kotlin/com/github/tomasmilata/intelliroutes/ControllerMethodCompletionContributor.kt
+++ src/main/kotlin/com/github/tomasmilata/intelliroutes/ControllerMethodCompletionContributor.kt
@@ -16,31 +16,39 @@ object ControllerMethodCompletionContributor {
                                 files: List<PsiClassOwner>) {
         val enteredText = parameters.position.text.removeSuffix("IntellijIdeaRulezzz ") // WTF?
 
-        val filesToSearch = files.filter { it.isValid && it.isPhysical }
-        val packageNames = filesToSearch.map { it.packageName }
-        val classes = filesToSearch.flatMap { it.classes.toList() }
-
         val playAction = playActionType(parameters)
 
-        fun suggest(suggestion: String, priority: Double) {
-            val lookupElement = LookupElementBuilder.create(suggestion)
+        fun suggest(lookupElement: LookupElementBuilder, priority: Double) {
             val prioritizedLookupElement = PrioritizedLookupElement.withPriority(lookupElement, priority)
             resultSet.addElement(prioritizedLookupElement)
         }
 
-        fun addMethodSuggestion(cls: PsiClass, method: PsiMethod) {
-            val fullMethod = "${cls.qualifiedName}.${method.name}"
-            val returnType = PsiTypesUtil.getPsiClass(method.returnType)
-            val priority = if (isInheritorOrSelf(returnType, playAction, true)) 1.0 else 0.0
-            suggest(fullMethod, priority)
+        val filesToSearch = files.filter { it.isValid && it.isPhysical }
+        filesToSearch.forEach { file ->
+            suggest(LookupElementBuilder.create(file.packageName), priority = 0.0)
         }
 
-        packageNames.forEach { suggest(it, priority = 0.0) }
-        classes.mapNotNull { it.qualifiedName }.forEach { suggest(it, priority = 0.0) }
+        val classes = filesToSearch.flatMap { it.classes.toList() }
+        classes.forEach {
+            it.qualifiedName?.let { className ->
+                val lookupElement = LookupElementBuilder
+                        .create(className).bold()
+                        .withIcon(it.getIcon(0))
+                suggest(lookupElement, priority = 0.0)
+            }
+        }
         classes.forEach { cls ->
             val className = cls.qualifiedName
             if (className != null && enteredText.startsWith(className)) {
-                cls.allMethods.forEach { method -> addMethodSuggestion(cls, method) }
+                cls.allMethods.forEach { method ->
+                    val fullMethod = "${cls.qualifiedName}.${method.name}"
+                    val returnType = PsiTypesUtil.getPsiClass(method.returnType)
+                    val priority = if (isInheritorOrSelf(returnType, playAction, true)) 1.0 else 0.0
+                    val lookupElement = LookupElementBuilder
+                            .create(fullMethod).bold()
+                            .withIcon(method.getIcon(0))
+                    suggest(lookupElement, priority)
+                }
             }
         }
     }
