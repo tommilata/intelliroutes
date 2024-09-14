fdc0e5284d07a611ce6eefcf84cb55f2139b7aef
Tomáš Milata <tomas.milata@gmail.com>
2024-09-14 22:14:55 +0200

    Migrate to new Platform plugin

--- build.gradle.kts
+++ build.gradle.kts
@@ -6,19 +6,41 @@ buildscript {
 
 plugins {
     id("java")
-    id("org.jetbrains.kotlin.jvm") version "1.9.21"
-    id("org.jetbrains.intellij") version "1.16.1"
+    id("org.jetbrains.kotlin.jvm") version "1.9.25"
+    id("org.jetbrains.intellij.platform") version "2.0.1"
+    // TODO remove?
+    id("org.jetbrains.intellij.platform.migration") version "2.0.1"
     id("com.palantir.git-version") version "3.0.0"
     id("org.jetbrains.grammarkit") version "2022.3.2.2"
 }
 
 repositories {
     mavenCentral()
+    intellijPlatform {
+        defaultRepositories()
+        intellijDependencies()
+    }
 }
 
 dependencies {
+    intellijPlatform {
+        intellijIdeaCommunity("2024.2.1")
+
+        plugins(listOf("org.intellij.scala:2024.2.25"))
+        bundledPlugins(listOf("com.intellij.java"))
+
+        instrumentationTools()
+
+
+    }
     implementation(kotlin("reflect", version = "1.7.22"))
     // explicit dependency to avoid version conflicts
+
+
+}
+
+kotlin {
+    jvmToolchain(17)
 }
 
 group = "com.github.tomas-milata"
@@ -33,41 +55,38 @@ sourceSets {
     }
 }
 
-intellij {
-    version.set("2024.1") // IntelliJ Platform IDE version used to build the plugin
-    plugins.set(listOf("com.intellij.java", "org.intellij.scala:2024.1.15"))
+intellijPlatform {
+
+    pluginConfiguration {
+        ideaVersion {
+            /** should be e.g. 203.* if sinceBuild is 202 (or 2020.2)
+             * to give compatibility with a next minor a shot */
+            untilBuild = "242.*"
+        }
+
+        changeNotes = """
+            <h1>2020.2.4</h1>
+            <ul>
+                <li>Added code completion and "go to definition" for other routers</li>
+                <li>Improved controller code completion performance</li>
+                <li>Added icons next to code completion suggestions</li>
+            </ul>
+            """.trimIndent()
+    }
+
+    publishing {
+        token = System.getenv("INTELLIJ_PUBLISH_TOKEN")
+        channels = listOf(System.getenv("INTELLIJ_PUBLISH_CHANNEL") ?: "alpha")
+    }
 }
 
 tasks {
-    patchPluginXml {
-        // sinceBuild defined by `intellij.version`
-
-        /** should be e.g. 203.* if sinceBuild is 202 (or 2020.2)
-         * to give compatibility with a next minor a shot */
-        untilBuild.set("241.*")
-
-        changeNotes.set(
-            """
-        <h1>2020.2.4</h1>
-        <ul>
-            <li>Added code completion and "go to definition" for other routers</li>
-            <li>Improved controller code completion performance</li>
-            <li>Added icons next to code completion suggestions</li>
-        </ul>
-        """
-        )
-    }
-
-    publishPlugin {
-        token.set(System.getenv("INTELLIJ_PUBLISH_TOKEN"))
-        channels.set(listOf(System.getenv("INTELLIJ_PUBLISH_CHANNEL") ?: "alpha"))
-    }
 
     generateParser {
         sourceFile.set(file("src/main/grammar/Routes.bnf"))
         pathToParser.set("/com/github/tomasmilata/intelliroutes/parser/RoutesParser.java")
         pathToPsiRoot.set("/com/github/tomasmilata/intelliroutes/psi")
-        targetRootOutputDir.set(file( "src/generated"))
+        targetRootOutputDir.set(file("src/generated"))
     }
 
     generateLexer {
@@ -79,4 +98,11 @@ tasks {
     compileKotlin {
         dependsOn(generateLexer)
     }
+}
+
+intellijPlatformTesting {
+    runIde
+    testIde
+    testIdeUi
+    testIdePerformance
 }
